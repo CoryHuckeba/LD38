e880a351e274c59b3ff9ad043cab449572f08980
Cory <cory@th.ru.st>
2017-04-22 23:13:58 -0400

    ASTEROIDS DONE LAUNCH OFF NOW

--- PlutosRevenge/Assets/Project/Scripts/AsteroidController.cs
+++ PlutosRevenge/Assets/Project/Scripts/AsteroidController.cs
@@ -34,14 +34,15 @@ public class AsteroidController : MonoBehaviour {
 
     public float minimumGravityRatio;   // The lowest effectiveness the gravitational effect can be
     public float maxGravityRange;       // Once the asteroid is this close the gravitational effects hit max (should help orbits?)
+    public float launchRange;
+    public float launchSpeed;
     public float maxSpeed;              // Top orbital speed
-    public float acceleration;          // How quickly speed increases when influenced
 
     public float randomAngle;           // Once spawned the direction the asteroid is facing will be randomly changed within this range (left and right)
 
     private float influenced;           // Ratio (0-1) of how much influence Pluto's gravity has on this asteroid
     private float distanceToPluto = 99f;
-    private float angleToPluto = 0f;
+    private bool launch = false;
 
     #endregion Properties & Variables
 
@@ -65,45 +66,50 @@ public class AsteroidController : MonoBehaviour {
         {
             // Set the distance to the player
             Vector3 vectorToPluto = PlutoController.Instance.transform.position - transform.position;
-            angleToPluto = Vector2.Angle(new Vector2(vectorToPluto.x, vectorToPluto.y), rb.velocity);
             distanceToPluto = Mathf.Abs(vectorToPluto.magnitude);
 
             // Determine if we're close enough to the player
             if (distanceToPluto < PlutoController.Instance.influence)
             {
-                // Calculate the effectiveness, then cap it at the minimum
-                float ratio = (maxGravityRange / distanceToPluto);
-                if (ratio < minimumGravityRatio) ratio = minimumGravityRatio;
+                if (distanceToPluto < launchRange && !launch)
+                    launch = true;
 
-                // Apply gravitational force to our momentum
-                rb.AddForce(vectorToPluto.normalized * gravity * ratio);
+                // Get the strength ratio
+                float ratio = Mathf.Min(maxGravityRange / distanceToPluto, 1); ;
+
+                // Add force in teh direction of pluto
+                rb.AddForce(new Vector2(vectorToPluto.x, vectorToPluto.y).normalized * ratio * PlutoController.Instance.gravityForce);
             }
         }
+        else if (launch)
+        {
+            // Launch
+            launch = false;
+            Launch();
+        }
+
 	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, PlutoController.Instance.transform.position);
+        Vector3 fuck = rb.velocity.normalized * 5f;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(fuck.x, fuck.y, 0));
+    }
 
     private void LateUpdate()
     {
-        // Apply more speed while we're in gravitational pull (if not at max)
-        //if (distanceToPluto < (PlutoController.Instance.influence / 2) && rb.velocity.magnitude < maxSpeed)
-        //{
-        //    float spd = rb.velocity.magnitude;
-        //    float rat = angleToPluto / 180;
-
-        //    Debug.Log("Angle to Pluto: " + angleToPluto + ", " + "Rat: " + rat);
-
-        //    spd = Mathf.Min(spd + (acceleration * Time.deltaTime) * rat, maxSpeed);
-        //    rb.velocity = rb.velocity.normalized * spd;
-        //}
+        
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // If this was another asteroid reduce our size by one    
-        if (collision.gameObject.tag == "Pluto")
-        {
-            PlutoController.Instance.AdjustHealth(stats.damage * -1);
-            Destroy(gameObject);
-        }
+        //// If this was another asteroid reduce our size by one    
+        //if (collision.gameObject.tag == "Pluto")
+        //{
+        //    PlutoController.Instance.AdjustHealth(stats.damage * -1);
+        //    Destroy(gameObject);
+        //}
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -117,4 +123,17 @@ public class AsteroidController : MonoBehaviour {
     }
 
     #endregion MonoBehaviour Implementation
+
+
+    #region Private Helpers
+
+    private void Launch()
+    {
+        // Trigger the speed boost
+        float spd = rb.velocity.magnitude;
+        rb.velocity = rb.velocity.normalized * (spd + launchSpeed);
+    }
+
+    #endregion Private Helpers
 }
+
